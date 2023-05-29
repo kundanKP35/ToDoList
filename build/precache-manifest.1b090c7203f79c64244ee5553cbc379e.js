ef8e571bd3c2f275563d7c3f967b4b126dbbab76
Kundan Prasad <kp800532@gmail.com>
2023-05-29 09:00:38 +0530

    initial commit

---
+++ build/precache-manifest.1b090c7203f79c64244ee5553cbc379e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab005751c2b757cdc346e38405d49c8f",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "3daf3a73d2343951cb22",
+    "url": "/ToDoList/static/js/2.5f702c95.chunk.js"
+  },
+  {
+    "revision": "da1a76da0c299e7a4b97",
+    "url": "/ToDoList/static/js/main.e40ff80a.chunk.js"
+  },
+  {
+    "revision": "c64a9f811fb2c545ba3f",
+    "url": "/ToDoList/static/js/runtime-main.76a3c940.js"
+  }
+]);
